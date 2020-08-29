dc30f306644538afa10da90c62b49b682751a17c
itexpert120 <itexpert120@gmail.com>
2020-08-29 06:00:28 +0200

    changes for q

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,3 +1,8 @@
 LOCAL_PATH := device/samsung/fortunaltedx
 PRODUCT_MAKEFILES := \
 	$(LOCAL_DIR)/lineage_fortunaltedx.mk
+
+COMMON_LUNCH_CHOICES := \
+	lineage_fortunaltedx-eng \
+	lineage_fortunaltedx-userdebug \
+	lineage_fortunaltedx-user
